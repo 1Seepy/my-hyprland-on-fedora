00c44ad2f7a1daf09ebe194019fc31ba71dde310
1Seepy <120071258+1Seepy@users.noreply.github.com>
2024-10-13 19:23:51 +0000

    Update install.sh
    -Added automatic cleanup
    -Automatically create wallpaper folder
    -Fix logout prompt

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@ echo "My Hyprland on Fedora will now install onto your system."
 read -p "Proceed (y/n)? " choice
 case "$choice" in 
   y|Y ) echo "Continuing.";;
-  n|N ) echo "Aborting.";;
+  n|N ) exit;;
   * ) echo "Invalid.";;
 esac
+echo "Updating..."
+sudo dnf update
+echo "Updated."
 echo "Installing Hyprland..."
 sudo dnf install hyprland
 sudo dnf install hyprland-devel
@@ -36,5 +39,15 @@ cd ~/Downloads/myfedorahyprland/dotfiles
 echo "If you have any configurations already setup, the terminal will now prompt you if you would like to replace them with these dotfiles. I would recommend replacing any defaults, but keeping any you made yourself."
 echo "If you don't get prompted to overwrite any configurations, you can ignore this message."
 mv -i * ~/.config
+mkdir ~/Pictures/Hyprpapers
+echo "Cleaning up..."
+cd ~/Downloads
+rm -rf myfedorahyprland
 echo "Done!"
+read -p "To enter Hyprland you have to logout. Logout now? " choice
+case "$choice" in 
+  y|Y ) sudo logout;;
+  n|N ) exit;;
+  * ) echo "Invalid.";;
+esac
 exit
