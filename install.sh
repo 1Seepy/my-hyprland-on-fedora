540c3ddb273a64a75b06c2a1e2bfc0f8b678d1a9
1Seepy <120071258+1Seepy@users.noreply.github.com>
2024-10-13 18:30:29 +0000

    Add install script

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+echo "My Hyprland on Fedora will now install onto your system."
+read -p "Proceed (y/n)? " choice
+case "$choice" in 
+  y|Y ) echo "Continuing.";;
+  n|N ) echo "Aborting.";;
+  * ) echo "Invalid.";;
+esac
+echo "Installing Hyprland..."
+sudo dnf install hyprland
+sudo dnf install hyprland-devel
+echo "Installed Hyprland."
+echo "Installing kitty..."
+sudo dnf install kitty
+echo "Installed kitty."
+echo "Installing fastfetch..."
+sudo dnf install fastfetch
+echo "Installed fastfetch."
+echo "Installing rofi..."
+sudo dnf install rofi-wayland
+echo "Installed rofi."
+echo "Installing waybar..."
+sudo dnf install waybar
+echo "Installed waybar."
+echo "Installing wlogout..."
+sudo dnf install wlogout
+echo "Installed wlogout."
+echo "Installing clipse..."
+sudo dnf install wl-clipboard
+sudo dnf copr enable azandure/clipse & echo "Enabling COPR repository..."
+sudo dnf install clipse
+echo "Cloning dotfiles..."
+mkdir ~/Downloads/myfedorahyprland
+git clone https://github.com/1Seepy/my-hyprland-on-fedora.git ~/Downloads/myfedorahyprland
+cd ~/Downloads/myfedorahyprland/dotfiles
+echo "If you have any configurations already setup, the terminal will now prompt you if you would like to replace them with these dotfiles. I would recommend replacing any defaults, but keeping any you made yourself."
+echo "If you don't get prompted to overwrite any configurations, you can ignore this message."
+mv -i * ~/.config
+echo "Done!"
+exit
