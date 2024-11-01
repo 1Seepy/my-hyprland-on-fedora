e9c06b0b45bb88d14b4b5ede666973028d90c503
1seepy <at.gould@outlook.com>
2024-10-31 21:44:37 -0400

    Add update script for Hyprland configuration
    Created a new script `update.sh` to clone the latest Hyprland configuration files from the repository. The script copies the files to the user's configuration directory and removes the temporary clone.

---
+++ dotfiles/hypr/scripts/update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git clone https://github.com/1seepy/my-hyprland-on-fedora.git ~/Downloads/myfedorahyprlandupdate1432
+cp -r ~/Downloads/myfedorahyprlandupdate1432/dotfiles/* ~/.config/
+rm -rf ~/Downloads/myfedorahyprlandupdate1432
