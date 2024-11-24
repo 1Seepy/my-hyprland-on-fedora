a11ef21ffc9ee45dde81faeeec708baa50a32bcd
1Seepy <at.gould@outlook.com>
2024-11-24 14:34:24 -0500

    use kellen style + small cleanups

--- dotfiles/rice/update.sh
+++ dotfiles/rice/update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git clone https://github.com/1seepy/my-hyprland-on-fedora.git ~/Downloads/myfedorahyprlandupdate1432
+git clone https://github.com/1seepy/my-hyprland-on-fedora.git -b kellen ~/Downloads/myfedorahyprlandupdate1432
 cp -r ~/Downloads/myfedorahyprlandupdate1432/dotfiles/* ~/.config/
 rm -rf ~/Downloads/myfedorahyprlandupdate1432
